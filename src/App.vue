7bb027d9fb450fe69f5956a12e1da85cb6e4d113
Thomas Bono <thomasbono.com@gmail.com>
2022-02-03 18:19:26 -0500

    using vite to run prime designer, having relatime path issues

--- src/App.vue
+++ src/App.vue
@@ -1,83 +1,101 @@
 <template>
-	<div :class="containerClass">
-		<AppTopBar v-if="codeEditor || initialized" @menubutton-click="onMenuButtonClick" />
-        <AppEditor v-if="!codeEditor && theme" :theme="theme" :active="sidebarActive" @compile="onCompile" @restart="onRestart" />
-        <div class="layout-content" v-if="codeEditor || initialized">
-			<router-view />
-		</div>
-        <div :class="['layout-mask', {'layout-mask-active': sidebarActive}]" @click="onMaskClick" v-if="!codeEditor && initialized"></div>
-        <AppFooter v-if="codeEditor || initialized" />
-        <transition name="fade">
-            <AppIntro @theme-select="onThemeSelect" v-if="!codeEditor && !initialized" />
-        </transition>
-	</div>
+  <div :class="containerClass">
+    <AppTopBar
+      v-if="codeEditor || initialized"
+      @menubutton-click="onMenuButtonClick"
+    />
+    <AppEditor
+      v-if="!codeEditor && theme"
+      :theme="theme"
+      :active="sidebarActive"
+      @compile="onCompile"
+      @restart="onRestart"
+    />
+    <div v-if="codeEditor || initialized" class="layout-content">
+      <router-view />
+    </div>
+    <div
+      v-if="!codeEditor && initialized"
+      :class="['layout-mask', { 'layout-mask-active': sidebarActive }]"
+      @click="onMaskClick"
+    ></div>
+    <AppFooter v-if="codeEditor || initialized" />
+    <transition name="fade">
+      <AppIntro
+        v-if="!codeEditor && !initialized"
+        @theme-select="onThemeSelect"
+      />
+    </transition>
+  </div>
 </template>
 
 <script>
-import AppIntro from './AppIntro';
-import AppTopBar from './AppTopbar';
-import AppEditor from './AppEditor';
-import AppFooter from './AppFooter';
+import AppIntro from './AppIntro.vue'
+import AppTopBar from './AppTopbar.vue'
+import AppEditor from './AppEditor.vue'
+import AppFooter from './AppFooter.vue'
 
 export default {
-    data() {
-        return {
-            sidebarActive: false,
-            theme: null,
-            initialized: false
-        }
-    },
-    themeStyle: null,
-    methods: {
-        onThemeSelect(theme) {
-            this.theme = theme;
+  components: {
+    AppTopBar: AppTopBar,
+    AppEditor: AppEditor,
+    AppFooter: AppFooter,
+    AppIntro: AppIntro,
+  },
+  data() {
+    return {
+      sidebarActive: false,
+      theme: null,
+      initialized: false,
+    }
+  },
+  themeStyle: null,
+  computed: {
+    containerClass() {
+      return [
+        'layout-wrapper',
+        {
+          'layout-editor-code': this.codeEditor,
+          'p-ripple-disabled': this.$primevue.config.ripple === false,
         },
-        onCompile(value) {
-            if (!this.initialized) {
-                this.initialized = true;
-            }
+      ]
+    },
+    codeEditor() {
+      // return import.meta.env.VITE_VUE_APP_EDITOR === 'code'
+      return true
+    },
+  },
+  methods: {
+    onThemeSelect(theme) {
+      this.theme = theme
+    },
+    onCompile(value) {
+      if (!this.initialized) {
+        this.initialized = true
+      }
 
-            var styleElement = document.createElement('style');
-            styleElement.type = 'text/css';
-            document.getElementsByTagName("head")[0].appendChild(styleElement);
-            
-            if (this.themeStyle) {
-                this.themeStyle.remove();
-            }
-                
-            this.themeStyle = styleElement;
-            this.themeStyle.appendChild(document.createTextNode(value));
-        },
-        onRestart() {
-            this.theme = null;
-            this.initialized = false;
-        },
-        onMenuButtonClick() {
-            this.sidebarActive = true;
-        },
-        onMaskClick() {
-            this.sidebarActive = false;
-        }
+      var styleElement = document.createElement('style')
+      styleElement.type = 'text/css'
+      document.getElementsByTagName('head')[0].appendChild(styleElement)
+
+      if (this.themeStyle) {
+        this.themeStyle.remove()
+      }
+
+      this.themeStyle = styleElement
+      this.themeStyle.appendChild(document.createTextNode(value))
     },
-    computed: {
-        containerClass() {
-            return [
-                'layout-wrapper', {
-                    'layout-editor-code': this.codeEditor,
-                    'p-ripple-disabled': this.$primevue.config.ripple === false
-                }
-            ]
-        },
-        codeEditor() {
-            return process.env.VUE_APP_EDITOR === 'code';
-        }
+    onRestart() {
+      this.theme = null
+      this.initialized = false
     },
-	components: {
-		'AppTopBar': AppTopBar,
-        'AppEditor': AppEditor,
-        'AppFooter': AppFooter,
-        'AppIntro': AppIntro
-	}
+    onMenuButtonClick() {
+      this.sidebarActive = true
+    },
+    onMaskClick() {
+      this.sidebarActive = false
+    },
+  },
 }
 </script>
 
