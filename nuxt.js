6cb7cf39835c973f04cb63fa382d872b53550be5
Thomas Bono <thomasbono.com@gmail.com>
2022-02-23 14:21:52 -0500

    added prefix to v2 only

--- nuxt.js
+++ nuxt.js
@@ -7,6 +7,7 @@ export default defineNuxtModule({
       // Add ./components dir to the list
       dirs.push({
         path: join(__dirname, 'v2/src/components'),
+        prefix: 'v2'
       })
       dirs.push({
         path: join(__dirname, 'v1/src/components'),
