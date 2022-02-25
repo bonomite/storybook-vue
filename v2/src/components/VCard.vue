47d54671ebb5711643ab1ddc9a28bcd139d43e86
Thomas Bono <thomasbono.com@gmail.com>
2022-02-25 15:44:34 -0500

    default width to number again

--- v2/src/components/VCard.vue
+++ v2/src/components/VCard.vue
@@ -128,7 +128,7 @@ const getMobileImageScale = computed(() => {
           :max-height="maxHeight"
           :allow-vertical-effect="allowVerticalEffect"
           :ratio="ratio"
-          :default-width="breakpoint[props.bp]"
+          :default-width="Number(breakpoint[props.bp])"
           role="presentation"
         />
         <!-- desktop, uses width and height props -->
